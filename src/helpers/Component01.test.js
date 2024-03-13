9ab0d013e9669c094eeeb892454a7495ab16fa08
Alvaroescumed <alvaro.escumed@gmail.com>
2024-03-13 16:01:16 +0100

    Update03

--- src/helpers/Component01.test.js
+++ src/helpers/Component01.test.js
@@ -1,15 +1,28 @@
-import { fireEvent, getByLabelText, getByText, render } from "@testing-library/react";
+import { fireEvent, getByRole, render } from "@testing-library/react";
 import Component01 from "./Component01";
 
 describe('Component01 shoul render a select with a different options and handle an onChange event ', () => {
 
-    const initialValue = 'option1';
-    const items = ['option1', 'option2', 'option3'];
-    const onChangeMock = jest.fn();
+    test('renders correctly', () => {
 
+        const initialValue = 'option1';
+        const items = ['option1', 'option2', 'option3'];
+        const onChangeMock = jest.fn();
 
-    it('Should render correctly', () => {
-        const select = render(<Component01 items={items} value={initialValue} onChange={onChangeMock}/>);
+        const { getByRole, getByDisplayValue, getByText} = render(<Component01 items={items} value={initialValue} onChange={onChangeMock}/>);
+
+        // comprobamos que se renderiza bien el componente
+            const element = getByRole('combobox'); //combobox es el elemento select
+            expect(element).toBeInTheDocument();
+
+        // comprobamo que las opciones se rendericen tambien bien
+
+        items.forEach(item => {
+            expect(getByText(item)).toBeInTheDocument();
+        })
+
+        // opcion inicial esta seleccionada
+
+        expect(getByDisplayValue(initialValue)).toBeInTheDocument();
     })
-
 })
